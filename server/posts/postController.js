514094e722ee7aa7447cfa9759893c187305bcc7
Thomas Hoffmann <thoff13@gmail.com>
2016-02-15 20:47:03 -0800

    Add addPost function to userController

--- server/posts/postController.js
+++ server/posts/postController.js
@@ -1,3 +1,13 @@
 var Post = require('./postModel');
 
-module.exports = 
+var addPost = function(req, res, next) {
+	var title = req.body.title;
+	var comment = req.body.comment;
+	Post.create({title: title, comment: comment}, function (err, post) {
+		if (err) {
+			res.send(400);
+		} else {
+			res.send(200);
+		}
+	}
+}
