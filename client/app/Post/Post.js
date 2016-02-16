2329a61eb793d58aa1c593dc41a7faa8c2866bec
Thomas Hoffmann <thoff13@gmail.com>
2016-02-15 23:10:21 -0800

    Successfully adds posts to database upon submission; middleware.js added;

--- client/app/Post/Post.js
+++ client/app/Post/Post.js
@@ -1,5 +1,17 @@
 angular.module('app.Post', [])
 
-.controller('PostController', function ($scope) {
-	$scope.data = {};
+.controller('PostController', function ($scope, $http) {
+	$scope.post = {};
+	$scope.addPost = function (post) {
+		return $http({
+      method: 'POST',
+      url: '/post',
+      data: post
+    })
+    .then(function(resp) {
+    	return resp;
+    }, function(resp) {
+    	return resp;
+    })
+	}
 });
--- server/config/routes.js
+++ server/config/routes.js
@@ -2,8 +2,7 @@ var postController = require('../posts/postController.js');
 // var helpers = require('./helpers.js'); // our custom middleware
 
 module.exports = function (app, express) {
-  console.log(postController);
-  app.post('/search', postController.addPost);
+  app.post('/post', postController.addPost);
 
   // app.get('/api/links/', linksController.allLinks);
   // app.post('/api/links/', linksController.newLink);
