4242e7e5c86297ee03a275bcf40330186a980763
Thomas Hoffmann <thoff13@gmail.com>
2016-03-17 22:55:39 -0700

    In process of getting join table for Tag and Post to work properly;

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,7 +3,7 @@ var userController = require('../controllers/userController.js');
 var passport = require('./authentication.js').passport;
 
 module.exports = function (app, express) {
-  // app.post('/post', postController.addPost);
+  app.post('/post', postController.addPost);
   // app.post('/search', postController.getPosts);
 
   app.post('/signup', userController.signup);
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,32 +1,44 @@
-// var Post = require('./postModel');
-// var User = require('../users/userModel');
+var db = require('../db');
 
-// module.exports = {
+module.exports = {
 
-// 	addPost: function(req, res, next) {
-// 		var title = req.body.title;
-// 		var comment = req.body.comment;
-// 		var tags = req.body.tags;
-// 		tags = tags.replace(/ /g,'').split(",");
-// 		Post.create({title: title, comment: comment, tags: tags}, function (err, post) {
-// 			if (err) {
-// 				res.send(400);
-// 			} else {
-// 				res.send(200);
-// 			}
-// 		});
-// 	},
+	addPost: function(req, res, next) {
+		var title = req.body.title;
+		var description = req.body.description;
+		var tags = req.body.tags;
+		tags = tags.replace(/ /g,'').split(",");
+		var tagModels = [];
+		for (var i = 0; i < tags.length; i++) {
+			tagModels.push({name: tags[i]});
+		}
+		db.Post.create({title: title, description: description})
+		.then(function(post) {
+			db.Tag.create(tagModels[0])
+			.then(function(tag) {
+				post.setTags([tag])
+				.then(function() {
+					res.status(201);
+				})
+			})
+			.catch(function() {
+				res.status(400);
+			});
+		})
+		.catch(function() {
+			res.status(400);
+		});
+	},
 
-// 	getPosts: function(req, res, next) {
-// 		var criteria = req.body.criteria;
-// 		criteria = criteria.replace(/ /g,'').split(",");
-// 		Post.find({tags: { $all: criteria} }).exec(function (err, data) {
-// 			if (err) {
-// 				res.send(400);
-// 			} else {
-// 				res.send(data);
-// 			}
-// 		})
-// 	}
+	getPosts: function(req, res, next) {
+		var criteria = req.body.criteria;
+		criteria = criteria.replace(/ /g,'').split(",");
+		db.Post.findAll({tags: { $all: criteria} }).exec(function (err, data) {
+			if (err) {
+				res.status(400);
+			} else {
+				res.send(data);
+			}
+		})
+	}
 
-// };
+};
