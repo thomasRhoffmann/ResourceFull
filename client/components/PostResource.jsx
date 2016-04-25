8504c0641dcce7453e9ab302c256fa017f1a267e
Thomas Hoffmann <thoff13@gmail.com>
2016-04-25 15:43:43 -0700

    Reinstate routes config to previous version; Add  barebones files for SignIn, SignUp, PostResource, and Search - (MVP app);

---
+++ client/components/PostResource.jsx
@@ -0,0 +1,1 @@
+AddResource.jsx
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,10 @@
-
+var postController = require('../controllers/postController.js');
+var userController = require('../controllers/userController.js');
 
 module.exports = function (app, express) {
+  // app.post('/post', postController.addPost);
+  // app.post('/search', postController.getPosts);
 
+  // app.post('/signup', userController.signup);
+  // app.post('/signin', userController.signin);
 };
