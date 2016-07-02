6dffc3efc3b2b2c23f1259e9612b2078afd52f11
Thomas Hoffmann <thoff13@gmail.com>
2016-07-02 14:41:22 -0700

    Add sign up logic to SignUp component;

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import {connect} from 'react-redux';
+import { sign_up } from '../actions/actions';
 
-export default class SignUp extends Component {
+class SignUp extends Component {
   render() {
+    var signUp = this.props.signUp;
     return (
       <div className='signup-container'>
         <h1 className='signup-header'>Sign Up</h1>
         <form className="signup-form">
-          <input className="signup-username" placeholder="username" type='text' />
-          <input className="signup-password" placeholder="password" type="password" />
-          <input type="submit" value="Sign Up" />
+          <input className='signup-username' placeholder='username' ref='username' type='text' />
+          <input className='signup-password' placeholder='password' ref='password' type='password' />
+          <input type='submit' value='Sign Up' onClick={() =>
+            signUp({
+              username: this.refs.username.getValue(),
+              password: this.refs.password.getValue()
+            })}
+          />
         </form>
         <Link to='/signin'>Already have an account? <strong>Sign In</strong> ...</Link>
       </div>
     )
   }
 }
+
+
+const mapStateToProps = (state) => {
+  return {
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (user) => {
+      dispatch(sign_up(user))
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
