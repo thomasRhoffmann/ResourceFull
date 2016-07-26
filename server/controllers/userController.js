1c493c1505721fe28940f9395e9f0d40025f4f89
Thomas Hoffmann <thoff13@gmail.com>
2016-07-26 16:37:31 -0700

    Add sign in route handler;

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,7 +41,51 @@ module.exports = {
   },
 
   signin: function(req, res, next) {
-    res.send('User signed in successfully!');
-  }
+    var username = req.body.username;
+    var password = req.body.password;
+
+    db.User.findOne({where: {username: username}})
+    .then(function(user) {
+      if (!user) {
+        res.send('username does not exist');
+        return;
+      }
+      bcrypt.compare(password, user.password, function(err, match) {
+        if (err) {
+          throw err;
+          res.sendStatus(500);
+          return;
+        }
+        if (!match) {
+          res.send('incorrect password'); 
+          return;
+        } else {
+          utils.createSession(req, res, user);
+        }
+      });
+    })
+    .catch(function(err) {
+      throw err;
+      res.sendStatus(500);
+    });
+  },
+
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
