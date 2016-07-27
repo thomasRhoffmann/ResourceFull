f1ed68de58aa13dc18d80b81859bb912d25ce9da
Thomas Hoffmann <thoff13@gmail.com>
2016-07-26 17:23:49 -0700

    Add sign in logic to SignIn component;

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
+import { connect } from 'react-redux';
 
-export default class SignIn extends Component {
+
+class SignIn extends Component {
   render() {
+    const signIn = (e) => {
+      const user = {
+        username: this.refs.username.value,
+        password: this.refs.password.value
+      };
+
+      this.props.signIn(user);
+    };
     return (
       <div className='signin-container'>
         <h1 className='signin-header'>Sign In</h1>
-        <form className="signin-form">
-          <input className="signin-username" placeholder="username" type='text' />
-          <input className="signin-password" placeholder="password" type="password" />
-          <input type="submit" value="Sign In" />
+        <form className="signin-form" >
+          <input className='signin-username' placeholder='username' ref='username' type='text' />
+          <input className='signin-password' placeholder='password' ref='password' type='password' />
+          <button type='button' onClick={signIn}>Sign In</button>
         </form>
-        <Link to='/signup'>Don't have an account yet? <strong>Sign Up</strong> ...</Link>
+        <Link to='/signin'>Already have an account? <strong>Sign In</strong> ...</Link>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (user) => {
+      dispatch(sign_up_async(user));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -6,7 +6,6 @@ import { sign_up_async } from '../actions/actions';
 class SignUp extends Component {
   render() {
     const signUp = (e) => {
-      e.preventDefault();
       const user = {
         username: this.refs.username.value,
         password: this.refs.password.value
@@ -17,10 +16,10 @@ class SignUp extends Component {
     return (
       <div className='signup-container'>
         <h1 className='signup-header'>Sign Up</h1>
-        <form className="signup-form" onSubmit={signUp}>
+        <form className="signup-form" >
           <input className='signup-username' placeholder='username' ref='username' type='text' />
           <input className='signup-password' placeholder='password' ref='password' type='password' />
-          <input type='submit' value='Sign Up' />
+          <button type='button' onClick={signUp}>Sign Up</button>
         </form>
         <Link to='/signin'>Already have an account? <strong>Sign In</strong> ...</Link>
       </div>
